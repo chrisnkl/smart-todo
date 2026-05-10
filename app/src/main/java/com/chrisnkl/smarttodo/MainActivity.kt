22c972214681684164b194c9ac54c110bccb7dde
chrisnkl <chrinikolaou.software@gmail.com>
2026-05-10 21:21:39 +0300

    initialize TodoViewModelFactory and update MainActivity to use TodoScreen

--- app/src/main/java/com/chrisnkl/smarttodo/MainActivity.kt
+++ app/src/main/java/com/chrisnkl/smarttodo/MainActivity.kt
@@ -4,25 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.chrisnkl.smarttodo.data.InsightRepository
 import com.chrisnkl.smarttodo.ui.theme.SmartTodoTheme
+import com.chrisnkl.smarttodo.ui.theme.TodoScreen
+import com.chrisnkl.smarttodo.viewmodel.TodoViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val repository = InsightRepository()
+        val factory = TodoViewModelFactory(repository)
+
         enableEdgeToEdge()
         setContent {
             SmartTodoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-
-                }
+                TodoScreen(factory = factory)
             }
         }
     }
